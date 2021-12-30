931ba947c719bbb816847cb62acff6b7fba03297
Jake Warren <jwarshack@gmail.com>
2021-12-30 10:42:04 -0800

    subgraph

--- hardhat/contracts/ElixirSoundLibrary.sol
+++ hardhat/contracts/ElixirSoundLibrary.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract ElixirSoundLibrary is ERC721URIStorage, ReentrancyGuard {
     using Counters for Counters.Counter;
 
-    Counters.Counter public tokenId;
+    Counters.Counter public tokenCounter;
 
     struct SoundData {
         uint256 price;
@@ -47,41 +47,44 @@ contract ElixirSoundLibrary is ERC721URIStorage, ReentrancyGuard {
     }
     
     function mintSound(SoundData memory _data) external nonReentrant {
-        uint256 currentId = tokenId.current();
+        uint256 currentId = tokenCounter.current();
         tokenIdToPrice[currentId] = _data.price;
+        
         _safeMint(msg.sender, currentId);
         _setTokenURI(currentId, _data.tokenURI);
-        tokenId.increment();        
+
+        tokenCounter.increment();
 
         emit SoundCreated(currentId, _data.tokenURI, _data.price, msg.sender);
     }
     
-    function licenseSound(uint256 _tokenId) external payable {
+    function licenseSound(uint256 _tokenId) external payable nonReentrant {
         require(!isLicensed[_tokenId][msg.sender], "Sound is already licensed");
         uint256 _price = tokenIdToPrice[_tokenId];
         uint256 _fee = _price / 50;
-        address _owner = ownerOf(_tokenId);
-        require(msg.sender != _owner, "Licensee cannot be the owner");
+        address _tokenOwner = ownerOf(_tokenId);
+        require(msg.sender != _tokenOwner, "Licensee cannot be the owner");
         require(msg.value == _price, "Please submit the correct amount of ether");
+
         tokenIdToLicensees[_tokenId].push(msg.sender);
         licenseeToTokenIds[msg.sender].push(_tokenId);
         isLicensed[_tokenId][msg.sender] = true;
 
-        payable(_owner).transfer(_price - _fee);
+        payable(_tokenOwner).transfer(_price - _fee);
         payable(owner).transfer(_fee);
 
-        emit SoundLicensed(_tokenId, _price, _owner, msg.sender);
+        emit SoundLicensed(_tokenId, _price, _tokenOwner, msg.sender);
     }
     
-    function sound(uint256 _tokenId) external view returns (uint256, uint256, string memory, address) {
-        return (_tokenId, tokenIdToPrice[_tokenId], tokenURI(_tokenId), ownerOf(_tokenId));
+    function sound(uint256 _tokenId) external view returns (uint256 tokenId, uint256 price, string memory _tokenURI, address tokenOwner, address[] memory licensees) {
+        return (_tokenId, tokenIdToPrice[_tokenId], tokenURI(_tokenId), ownerOf(_tokenId), tokenIdToLicensees[_tokenId]);
     }
 
     function licenses() external view returns (uint256[] memory) {
         return licenseeToTokenIds[msg.sender];
     }
 
-    function updatePriceMapping(uint256 _tokenId, uint256 _price) external {
+    function updatePrice(uint256 _tokenId, uint256 _price) external {
         require(msg.sender == ownerOf(_tokenId), "Only the owner can update the price");
         tokenIdToPrice[_tokenId] = _price;
 
--- hardhat/test/test.js
+++ hardhat/test/test.js
@@ -1,20 +1,20 @@
-// const { expect } = require("chai");
-// const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-// describe("ERC721 Test", function () {
+describe("Elixir Sound Library test", function () {
 
-//     it('should mint and increment counter', async () => {
-//         const [_, minter] = await hre.ethers.getSigners()
+    it('should mint and increment counter', async () => {
+        const [_, minter] = await hre.ethers.getSigners()
 
-//         const NFT = await hre.ethers.getContractFactory("TestNFT");
-//         let nft = await NFT.deploy();
+        const NFT = await hre.ethers.getContractFactory("TestNFT");
+        let nft = await NFT.deploy();
       
-//         await nft.deployed();
+        await nft.deployed();
 
-//         await nft.connect(minter).mint(hre.ethers.utils.parseEther('0.1'), "sdfdsfsdfsd")
+        await nft.connect(minter).mint(hre.ethers.utils.parseEther('0.1'), "sdfdsfsdfsd")
 
 
 
-//     })
+    })
 
-// })
+})
