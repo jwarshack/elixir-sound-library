6680472a4d95bcde73d35d4cef2abfc5b3f32a56
Jake Warren <jwarshack@gmail.com>
2022-01-12 06:35:45 -0800

    popular link

--- front-end/components/Hero.js
+++ front-end/components/Hero.js
@@ -5,7 +5,7 @@ import React from 'react'
 
 export default function Hero() {
     return (
-        <Grid bgImage="../public/img/hero.png"
+        <Grid
             templateColumns={{sm:"repeat(1, 1fr)", md: "repeat(2, 1fr)"}}
             p={10}
             gap={10}
@@ -14,7 +14,7 @@ export default function Hero() {
             maxW="600px"
         >
             <NextLink href="/browse"><Button as="a"  w="100%" h={{base: "100px", md: "200px"}} cursor="pointer" rounded="xl" fontSize="xl" bgGradient="linear(to-r, pink.400, pink.500)"  _hover={{bg: "pink.300"}}>Browse</Button></NextLink>
-            <NextLink href="/"><Button as="a" w="100%" h={{base: "100px", md: "200px"}} cursor="pointer" rounded="xl" fontSize="xl" bgGradient="linear(to-r, pink.400, pink.500)" _hover={{bg: "pink.300"}}>Popular</Button></NextLink>
+            <NextLink href="/popular"><Button as="a" w="100%" h={{base: "100px", md: "200px"}} cursor="pointer" rounded="xl" fontSize="xl" bgGradient="linear(to-r, pink.400, pink.500)" _hover={{bg: "pink.300"}}>Popular</Button></NextLink>
             <NextLink href="/my-sounds"><Button as="a" w="100%" h={{base: "100px", md: "200px"}} cursor="pointer"  rounded="xl" fontSize="xl" bgGradient="linear(to-r, pink.400, pink.500)" _hover={{bg: "pink.300"}}>My Sounds</Button></NextLink>
             <NextLink href="/create"><Button as="a" w="100%" h={{base: "100px", md: "200px"}}  cursor="pointer"  rounded="xl" fontSize="xl" bgGradient="linear(to-r, pink.400, pink.500)" _hover={{bg: "pink.300"}}>Create</Button></NextLink>
 
