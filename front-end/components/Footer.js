f1694e635fd3b67e55ccbee2535a126748582836
Jake Warren <jwarshack@gmail.com>
2022-01-11 20:05:19 -0800

    discord link

--- front-end/components/Footer.js
+++ front-end/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
         <Flex bg="black" color="white" justify="center" align="center" p={5}>
             <HStack fontSize="4xl" spacing={4}>
                 <Link href="https://twitter.com/ElixirSoundLib" target="_blank" rel="noopener noreferrer"><FaTwitter/></Link>
-                <Link href="https://twitter.com/ElixirSoundLib" target="_blank" rel="noopener noreferrer"><FaDiscord/></Link>
+                <Link href="https://discord.gg/XjE3bzKNcT" target="_blank" rel="noopener noreferrer"><FaDiscord/></Link>
             </HStack>    
         </Flex>
     )
