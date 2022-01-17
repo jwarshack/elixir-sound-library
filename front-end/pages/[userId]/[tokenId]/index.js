f5960a64e10c06a6cda8f1c587797ed702c0e237
Jake Warren <jwarshack@gmail.com>
2022-01-17 12:22:19 -0800

    mobile sound page

--- front-end/pages/[userId]/[tokenId]/index.js
+++ front-end/pages/[userId]/[tokenId]/index.js
@@ -2,7 +2,8 @@
 import React, { useState, useEffect } from 'react'
 import { ethers } from 'ethers'
 import Head from 'next/head'
-import { Box, Flex, Text, Spinner, Button, useToast} from '@chakra-ui/react'
+import { Box, Flex, Text, Spinner, Button, useToast, Grid} from '@chakra-ui/react'
+import { useMediaQuery } from '@chakra-ui/react'
 import dynamic from 'next/dynamic'
 import { getAddressOrENS } from '../../../utils/helpers'
 import NextLink from 'next/link'
@@ -23,10 +24,9 @@ export default function Index(props) {
     const [isLoading, setIsLoading] = useState(false)
     const toast = useToast()
 
+    const [isMobile] = useMediaQuery("(max-width: 768px)") 
 
     const { web3Provider } = useWeb3()
-
-
 
     const AudioPlayer = dynamic(
         () => import('../../../components/AudioPlayer'),
@@ -95,34 +95,31 @@ export default function Index(props) {
             <Head>
                 <title> {props.token.name} - {props.token.creator} | Elixir Sound Library</title>
             </Head>
-            <Flex direction="column" p={10}>
-                <Flex justify="center" w="100%" maxWidth="1000px" mx="auto">
-                    <Box w="50%" rounded="xl" overflow="hidden" border="1px" borderColor="gray.200" mr={6} pb={10}>
-                        <AudioPlayer src={props.token.tokenURI}/>
-                    </Box>
+            <Grid
+                templateColumns={{sm:"repeat(1, 1fr)", md: "repeat(2, 1fr)"}}
+                gap={8}
+                p={10}
+                >
+                <Box w="100%" rounded="xl" overflow="hidden" border="1px" borderColor="gray.200" mr={6} pb={10}>
+                    <AudioPlayer src={props.token.tokenURI}/>
+                </Box>
 
-                    <Flex direction="column" w="50%">
-                        <Text fontWeight="semibold" fontSize="3xl">{props.token.name}</Text>
-                        <Text fontSize="xl" fontWeight="semibold" py={3}>Total Samples: <Text as="span" mr={8} fontSize="xl" fontWeight="normal" textColor="gray.500">{props.token.licenseCount}</Text></Text>
-                        <Text>File type: {props.token.type}</Text>
-                    </Flex>
-                    
+                <Flex direction="column" w="100%">
+                    <Text fontWeight="semibold" fontSize="3xl">{props.token.name}</Text>
+                    <Text fontSize="xl" fontWeight="semibold" py={3}>Total Samples: <Text as="span" mr={8} fontSize="xl" fontWeight="normal" textColor="gray.500">{props.token.licenseCount}</Text></Text>
+                    <Text>File type: {props.token.type}</Text>
                 </Flex>
-                <Flex w="100%" maxWidth="1000px" pt={10} mx="auto">
-                    <Flex p={5} direction="column" align="start" w="50%" rounded="xl" overflow="hidden" border="1px" borderColor="gray.200" mr={6} >
-                        <Text fontSize="2xl" fontWeight="semibold">Created by: <Text as="span" mr={8} fontSize="xl" fontWeight="normal" textColor="gray.500">{user}</Text></Text>
-                        <NextLink href={`/${encodeURIComponent(props.token.creator)}`}><Button mt={5}>See more by this artist</Button></NextLink>
-                    </Flex>
-                    <Flex direction="column" w="50%" align="start" p={5} rounded="xl" overflow="hidden" border="1px" borderColor="gray.300" bg="gray.200" >
-                            <Text>Price:</Text>
-                            <Text>{props.token.price} ETH</Text>
-                            <Button bg="black" color="white" _hover={{bg: "gray.600"}} mt={5} px={4} onClick={() => licenseSound(props.token.tokenId)}>Sample</Button>
-
-                    </Flex>
-
-                    
+                <Flex p={5} direction="column" align="start" w="100%" rounded="xl" overflow="hidden" border="1px" borderColor="gray.200" mr={6} >
+                    <Text fontSize="2xl" fontWeight="semibold">Created by: <Text as="span" mr={8} fontSize="xl" fontWeight="normal" textColor="gray.500">{user}</Text></Text>
+                    <NextLink href={`/${encodeURIComponent(props.token.creator)}`}><Button mt={5}>See more by this artist</Button></NextLink>
                 </Flex>
-            </Flex>
+                <Flex direction="column" w="100%" align="start" p={5} rounded="xl" overflow="hidden" border="1px" borderColor="gray.300" bg="gray.200" >
+                        <Text>Price:</Text>
+                        <Text>{props.token.price} ETH</Text>
+                        <Button bg="black" color="white" _hover={{bg: "gray.600"}} mt={5} px={4} onClick={() => licenseSound(props.token.tokenId)}>Sample</Button>
+
+                </Flex>
+            </Grid>
         </>
 
     )
