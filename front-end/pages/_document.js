ad01d01c5c885cb28fff055a29615d8ed6310ef3
Jake Warren <jwarshack@gmail.com>
2022-01-20 19:05:34 -0800

    SEO

--- front-end/pages/_document.js
+++ front-end/pages/_document.js
@@ -8,7 +8,9 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta name="description" content="The first decentralized music sample marketplace. Live on the Arbitrum network! Mint sounds and license them out to other artists. Search through a library of samples." />
+        </Head>
         <body>
           <Main />
           <NextScript />
